eee804d8cc2a939525bed2d730a69f0bb9abd0f6
Athar Ansari <athar8932@gmail.com>
2024-08-06 20:02:05 +0530

    update

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <>
       <hr className="border-1 border-black mx-10 mt-8  xs:mx-4  xs:mt-2" />
-      <p className="text-base flex justify-center items-center p-2 font-lora font-medium xs:text-[0.65rem] xs:p-3  ">
+      <p className="text-base flex justify-center items-center p-2 font-lora font-medium xs:text-[0.5rem] xs:p-2  ">
         Lost & Found, © 2024 Inc. All rights reserved, Design By{" "}
         <a href="https://www.atharansari.me/">
           <HoverCardDemo />
